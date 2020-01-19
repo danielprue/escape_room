4f46b92af541b1422c5f679715010765f6a31f26
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-01-19 14:10:41 -0800

    tiles change state appropriately

--- components/controller/index.js
+++ components/controller/index.js
@@ -1,48 +1,128 @@
 let control = {
+    tile_count: 16,
+    tileDim: Math.sqrt(this.tile_count),
+    activeTiles: [],
+    inactiveTiles: [],
+    clearTiles: [],
     emptyTiles: [],
     doorTile: -1,
     keyTile: -1,
     hasKey: false,
 }
 
+control.tileDim = Math.sqrt(control.tile_count);
 control.createRoom = function createRoom(){
     const room = document.createElement('div');
     room.classList.add('room');
 
-    const tile_count = 9;
-    randomizeLoot(tile_count);
-    for(let i = 0; i < tile_count; i++){
-        let tileClass = '';
-        if(i === control.doorTile)
-            tileClass = 'doorTile'
-        else if(i === control.keyTile)
-            tileClass = 'keyTile'
-        else
-            tileClass = 'emptyTile'
-        room.append(createTile(tileClass))
-    }
+    document.querySelector('.start-btn').style.display = 'none';
 
+    randomizeLoot(control.tile_count);
+    for(let i = 0; i < control.tile_count; i++){
+        room.append(createTile(control.tileDim, i))
+    }
     document.querySelector('.game').append(room);
+    setDoor();
     return room;
 }
 
-function createTile(tileClass){
+function createTile(tileDim, tileNo){
     const tile = document.createElement('div');
-    tile.classList.add('tile', tileClass);
+    const tileWidth = 100 / tileDim;
+    tile.classList.add('tile', 'covered', `tile_${tileNo}`);
+    tile.style.width = `${tileWidth}%`
+
+    // tile.addEventListener('click', clickActive(this, tileNo))
+    // () => {
+    //     if(title.classList.contains('activeTile')){
+    //         title.classList.toggle('activeTile');
+    //         control.clearTiles.push(tileNo);
+    //         if(title.classList.contains(`${control.keyTile}`)){
+    //             title.classList.toggle('keyTile');
+    //         }else{
+    //             title.classList.toggle('clearTile')
+    //         }
+    //         refreshActive();
+    //     }
+    //})
 
     return tile;
 }
 
 function randomizeLoot(roomSize){
     control.emptyTiles = [...Array(roomSize).keys()];
+    control.inactiveTiles = [...Array(roomSize).keys()];
     control.doorTile = control.emptyTiles[Math.floor(Math.random()*control.emptyTiles.length)];
-    removeTile(control.doorTile);
+    removeEmpty(control.doorTile);
+    removeInactive(control.doorTile);
     control.keyTile = control.emptyTiles[Math.floor(Math.random()*control.emptyTiles.length)];
-    removeTile(control.keyTile);
+    removeEmpty(control.keyTile);
 }
 
-function removeTile(tileNo){
+function removeEmpty(tileNo){
     let index = control.emptyTiles.indexOf(tileNo);
     if(index > -1)
         control.emptyTiles.splice(index, 1);
 }
+
+function refreshActive(){
+    for(let i = 0; i < control.inactiveTiles.length; i++){
+        if( (control.clearTiles.includes(control.inactiveTiles[i] + 1) && ( (control.inactiveTiles[i] + 1) % control.tileDim != 0 ) ) || 
+             control.clearTiles.includes(control.inactiveTiles[i] + control.tileDim) ||
+            (control.clearTiles.includes(control.inactiveTiles[i] - 1) && control.inactiveTiles[i] % control.tileDim != 0) || 
+             control.clearTiles.includes(control.inactiveTiles[i] - control.tileDim)){
+                control.activeTiles.push(control.inactiveTiles[i]);
+                setActive(control.inactiveTiles[i]);
+           }
+    }
+    removeInactive();
+}
+
+function removeInactive(){
+    for(let i = 0; i < control.activeTiles.length; i++){
+        if(control.inactiveTiles.includes(control.activeTiles[i])){
+            control.inactiveTiles.splice(control.inactiveTiles.indexOf(control.activeTiles[i]), 1);
+            const active = document.querySelector(`.tile_${control.activeTiles[i]}`)
+            active.classList.toggle('activeTile');
+            active.classList.toggle('covered');
+        }
+    }
+}
+
+function setDoor(){
+    const door = document.querySelector(`.tile_${control.doorTile}`);
+    control.inactiveTiles.splice(control.inactiveTiles.indexOf(control.doorTile), 1);
+    door.classList.toggle('covered');
+    door.classList.toggle('doorTile');
+    control.clearTiles.push(control.doorTile);
+    refreshActive();
+}
+
+function setActive(tileNo){
+    const tile = document.querySelector(`.tile_${tileNo}`);
+    tile.addEventListener('click', () => {
+        if(tile.classList.contains('activeTile')){
+            tile.classList.toggle('activeTile');
+            control.clearTiles.push(tileNo);
+
+            if(tileNo === control.keyTile)
+                tile.classList.toggle('keyTile');
+            else
+                tile.classList.toggle('clearTile');
+            refreshActive();
+        }
+    })
+}
+
+// function clickActive(tile, tileNo){
+//     if(tile.classList.contains('activeTile')){
+//         tile.classList.toggle('activeTile');
+//         control.clearTiles.push(tileNo);
+//     }
+//     if(tile.classList.contains(`tile_${control.keyTile}`)){
+//         tile.classList.toggle('keyTile');
+//     }else{
+//         tile.classList.toggle('clearTile')
+//     }
+//     refreshActive();
+// }
